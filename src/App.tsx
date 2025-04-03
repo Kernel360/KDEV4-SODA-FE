ab192e8f3fa3bc7231026bf210fb38b07f224c22
seoyeon-jung <jungluchia1204@gmail.com>
2025-04-03 17:38:14 +0900

    feat: 계정 상세 정보 페이지 추가 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -11,102 +11,116 @@ import {
   Project,
   EditProject,
   AccountList,
+  AdminAccountDetail,
   CreateAccount,
   CompanyList,
   Company,
   EditCompany,
   UserMain,
-  UserProject
+  UserProject,
+  UserAccountDetail
 } from './pages'
 import { ThemeProvider } from '@mui/material/styles'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import theme from './theme'
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Routes>
-        {/* Auth routes without layout */}
-        <Route
-          path="/"
-          element={<Login />}
-        />
-        <Route
-          path="/reset-password"
-          element={<ResetPassword />}
-        />
-        <Route
-          path="/user-info"
-          element={<UserInfo />}
-        />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Routes>
+          {/* Auth routes without layout */}
+          <Route
+            path="/"
+            element={<Login />}
+          />
+          <Route
+            path="/reset-password"
+            element={<ResetPassword />}
+          />
+          <Route
+            path="/user-info"
+            element={<UserInfo />}
+          />
 
-        {/* Admin routes with layout */}
-        <Route
-          path="/admin"
-          element={
-            <Layout>
-              <Outlet />
-            </Layout>
-          }>
+          {/* Admin routes with layout */}
           <Route
-            index
-            element={<AdminMain />}
-          />
-          <Route
-            path="projects"
-            element={<ProjectList />}
-          />
-          <Route
-            path="projects/create"
-            element={<CreateProject />}
-          />
-          <Route
-            path="projects/:id"
-            element={<Project />}
-          />
-          <Route
-            path="projects/:id/edit"
-            element={<EditProject />}
-          />
-          <Route
-            path="accounts"
-            element={<AccountList />}
-          />
-          <Route
-            path="accounts/create"
-            element={<CreateAccount />}
-          />
-          <Route
-            path="companies"
-            element={<CompanyList />}
-          />
-          <Route
-            path="companies/:id"
-            element={<Company />}
-          />
-          <Route
-            path="companies/:id/edit"
-            element={<EditCompany />}
-          />
-        </Route>
+            path="/admin"
+            element={
+              <Layout>
+                <Outlet />
+              </Layout>
+            }>
+            <Route
+              index
+              element={<AdminMain />}
+            />
+            <Route
+              path="projects"
+              element={<ProjectList />}
+            />
+            <Route
+              path="projects/create"
+              element={<CreateProject />}
+            />
+            <Route
+              path="projects/:id"
+              element={<Project />}
+            />
+            <Route
+              path="projects/:id/edit"
+              element={<EditProject />}
+            />
+            <Route
+              path="accounts"
+              element={<AccountList />}
+            />
+            <Route
+              path="accounts/:id"
+              element={<AdminAccountDetail />}
+            />
+            <Route
+              path="accounts/create"
+              element={<CreateAccount />}
+            />
+            <Route
+              path="companies"
+              element={<CompanyList />}
+            />
+            <Route
+              path="companies/:id"
+              element={<Company />}
+            />
+            <Route
+              path="companies/:id/edit"
+              element={<EditCompany />}
+            />
+          </Route>
 
-        {/* User routes with layout */}
-        <Route
-          path="/user"
-          element={
-            <Layout>
-              <Outlet />
-            </Layout>
-          }>
+          {/* User routes with layout */}
           <Route
-            index
-            element={<UserMain />}
-          />
-          <Route
-            path="projects/:id"
-            element={<UserProject />}
-          />
-        </Route>
-      </Routes>
+            path="/user"
+            element={
+              <Layout>
+                <Outlet />
+              </Layout>
+            }>
+            <Route
+              index
+              element={<UserMain />}
+            />
+            <Route
+              path="projects/:id"
+              element={<UserProject />}
+            />
+            <Route
+              path="account"
+              element={<UserAccountDetail />}
+            />
+          </Route>
+        </Routes>
+      </LocalizationProvider>
     </ThemeProvider>
   )
 }
