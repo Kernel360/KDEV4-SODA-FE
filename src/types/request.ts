fb545dbc649405a8807f7b663e8f053a2ffe9760
yoodab <yoondab2166@gmail.com>
2025-04-11 05:10:43 +0900

    feat: request 타입 정의 추가 (#28)

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,6 +1,9 @@
+import { ReactNode } from "react"
+
 export type RequestStatus = '승인대기중' | '승인됨' | '반려됨'
 
 export interface Request {
+  content: ReactNode
   id: number
   title: string
   description: string
